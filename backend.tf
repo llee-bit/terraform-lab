aa7fae51ebdc4307fd2dee198a79026319f3c7a9
llee-bit <leeloretta@yahoo.com>
2025-10-03 13:41:01 -0700

    add files

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "terraform-state2-rg"
-    storage_account_name = "tfstatellee001"
-    container_name       = "tfstate"
+    resource_group_name  = "terraform-state-rg"
+    storage_account_name = "tfstateobay001"
+    container_name       = "loretta"
     key                  = "terraform.tfstate"
     use_azuread_auth     = true
   }
--- main.tf
+++ main.tf
@@ -3,7 +3,11 @@ terraform {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
-    }    
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
   }
   required_version = ">=1.0.0"
 }
@@ -11,8 +15,19 @@ terraform {
 provider "azurerm" {
   features {}
 }
-
-resource "azurerm_resource_group" "rg" {
-  name     = "test-rg"
+resource "azurerm_resource_group" "rg2" {
+  name     = "test-rg-Loretta"
   location = "westus2"
 }
+
+# resource "azurerm_storage_account" "sa" {
+#   name                     = var.storage_account_name
+#   resource_group_name      = azurerm_resource_group.rg2.name
+#   location                 = azurerm_resource_group.rg2.location
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
+#   allow_shared_key_access  = true
+#   min_tls_version          = "TLS1_2"
+# }
+
+
